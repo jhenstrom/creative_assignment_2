8d4b86a6b23dcbe9f32e4692eb6827bd28a21d86
EC2 Default User <ec2-user@ip-172-31-6-192.us-east-2.compute.internal>
2018-10-04 00:32:20 +0000

    stuff

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ function one_month(stock)
             var o_date = parsed_json[0]['date'];
             var closing = parsed_json[parsed_json.length - 1]['close'];
             var c_date = parsed_json[parsed_json.length - 1]['date'];
-            var change, change_sign = diff(opening, closing);
-            everything+="<p>Over the last month " + stock + " had a low point of " + low + "and a high point of " + high + ".";
-            everything +="They had a starting price of " + opening + " on " + o_date + " and a ending price of " + closing + " on " + c_date + " yeilding a " + change_sign + " of $" + change + ".</p>";
+            var change = diff(opening, closing);
+            everything+="<p>Over the last month " + stock + " had a low point of $" + low + " and a high point of $" + high + ".";
+            everything +="They had a starting price of $" + opening + " on " + o_date + " and a ending price of $" + closing + " on " + c_date + " yeilding a " + change[1] + " of $" + change[0] + ".</p>";
             $("#results").html(everything);
         }
     })
@@ -70,9 +70,9 @@ function one_day(stock)
             var o_time = parsed_json[0]['label'];
             var closing = parsed_json[parsed_json.length - 1]['close'];
             var c_time = parsed_json[parsed_json.length - 1]['label'];
-            var change, change_sign = diff(opening, closing);
-            everything+="<p>Today " + stock + " had a low point of " + low + "and a high point of " + high + ".";
-            everything +="They had a starting price of " + opening + " at " + o_time + " and a ending price of " + closing + " at " + c_time + " yeilding a " + change_sign + " of $" + change + ".</p>";
+            var change = diff(opening, closing);
+            everything+="<p>Today " + stock + " had a low point of $" + low + " and a high point of $" + high + ".";
+            everything +="They had a starting price of $" + opening + " at " + o_time + " and a ending price of $" + closing + " at " + c_time + " yeilding a " + change[1] + " of $" + change[0] + ".</p>";
             $("#results").html(everything);
         }
     })
@@ -93,9 +93,9 @@ function three_month(stock)
             var o_date = parsed_json[0]['date'];
             var closing = parsed_json[parsed_json.length - 1]['close'];
             var c_date = parsed_json[parsed_json.length - 1]['date'];
-            var change, change_sign = diff(opening, closing);
-            everything+="<p>Over the last three months " + stock + " had a low point of " + low + "and a high point of " + high + ".";
-            everything +="They had a starting price of " + opening + " on " + o_date + " and a ending price of " + closing + " on " + c_date + " yeilding a " + change_sign + " of $" + change + ".</p>";
+            var change = diff(opening, closing);
+            everything+="<p>Over the last three months " + stock + " had a low point of $" + low + " and a high point of $" + high + ".";
+            everything +="They had a starting price of $" + opening + " on " + o_date + " and a ending price of $" + closing + " on " + c_date + " yeilding a " + change[1] + " of $" + change[0] + ".</p>";
             $("#results").html(everything);
         }
     })
@@ -111,14 +111,14 @@ function six_month(stock)
         success: function(parsed_json)
         {
             var everything ="<h4>You selected a SIX MONTH report:</h4>";
-            var low, high = get_low_high(parsed_json);
+            var stats = get_low_high(parsed_json);
             var opening = parsed_json[0]['open'];
             var o_date = parsed_json[0]['date'];
             var closing = parsed_json[parsed_json.length - 1]['close'];
             var c_date = parsed_json[parsed_json.length - 1]['date'];
-            var change, change_sign = diff(opening, closing);
-            everything+="<p>Over the last six months " + stock + " had a low point of " + low + "and a high point of " + high + ".";
-            everything +="They had a starting price of " + opening + " on " + o_date + " and a ending price of " + closing + " on " + c_date + " yeilding a " + change_sign + " of $" + change + ".</p>";
+            var change = diff(opening, closing);
+            everything+="<p>Over the last six months " + stock + " had a low point of $" + stats[0] + " and a high point of $" + stats[1] + ".";
+            everything +="They had a starting price of $" + opening + " on " + o_date + " and a ending price of $" + closing + " on " + c_date + " yeilding a " + change[1] + " of $" + change[0] + ".</p>";
             $("#results").html(everything);
         }
     })
@@ -139,7 +139,7 @@ function get_low_high(json)
             best_so_far = json[i]['high'];
         }
     }
-    return worst_so_far, best_so_far;
+    return [worst_so_far, best_so_far];
 }
 
 function book(stock)
@@ -153,13 +153,13 @@ function book(stock)
         {
             var open = parsed_json['quote']['open'];
             var close = parsed_json['quote']['close'];
-            var change, sign = diff(open, close);
-            var ytd, ytd_sign = getytd(parsed_json['quote']['ytdChange']);
+            var change= diff(open, close);
+            var ytd = getytd(parsed_json['quote']['ytdChange']);
             var everything ="<h4>You selected a BOOK report for " + parsed_json['quote']['companyName'] + ":</h4>";
             everything+="<p>Today " + stock + " had a low point of $" + parsed_json['quote']['low'] + " and a high point of $" + parsed_json['quote']['high'] + ". ";
-            everything +="They had a opening price of $" + open + " and a ending price of $" + close + " yeilding a " + sign + " of $" + change + ". ";
+            everything +="They had a opening price of $" + open + " and a ending price of $" + close + " yeilding a " + change[1] + " of $" + change[0] + ". ";
             everything += " They had a 52 week high of " + parsed_json['quote']['week53High'] + " and a 52 week low of " + parsed_json['quote']['week53Low'] + ". ";
-            everything += "Year-to-Day change is $ " + ytd + " so the stock is looking " + ytd_sign +".";
+            everything += "Year-to-Day change is $ " + ytd[0] + " so the stock is looking " + ytd[1] +".";
             $("#results").html(everything);
         }
     })
@@ -169,15 +169,15 @@ function getytd(number)
 {
     if(number < 0)
     {
-        return number, "weak";
+        return [number, "weak"];
     }
     else if(number > 0)
     {
-        return number, "strong";
+        return [number, "strong"];
     }
     else if(number == 0)
     {
-         return number, "neutral";
+         return [number, "neutral"];
     }
 }
 
@@ -187,14 +187,14 @@ function diff(first, second)
     if(temp < 0)
     {
         temp = temp + (2*temp);
-        return temp, "increase";
+        return [temp, "increase"];
     }
     else if(temp > 0)
     {
-        return temp, "decrease";
+        return [temp, "decrease"];
     }
     else if(temp == 0)
     {
-        return temp, "constant";
+        return [temp, "constant"];
     }
 }
